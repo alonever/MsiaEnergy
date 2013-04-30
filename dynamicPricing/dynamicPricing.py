32d08a137acfec6054e8761783a1f79483fb291e
alonever <robie9705@gmail.com>
2013-04-29 21:04:04 -0500

    read_data

--- dynamicPricing/dynamicPricing.py
+++ dynamicPricing/dynamicPricing.py
@@ -1,5 +1,16 @@
-import pandas as pd
+# import pandas as pd
+# 
+# data = pd.read_csv('D:/MSIA/Courses/Spring13/MSIA 490/Week 5/DP_usage.csv', index_col = 'Hour')
 
-data = pd.read_csv('D:/MSIA/Courses/Spring13/MSIA 490/Week 5/DP_usage.csv', index_col = 'Hour')
+import csv
+import numpy as np
 
-print data[:24]
+def read_data():
+    with open('D:/MSIA/Courses/Spring13/MSIA 490/Week 5/DP_usage.csv', 'r') as raw:
+        reader = csv.reader(raw)
+        header = reader.next()
+        data = []
+        for row in reader:
+            data.append(row)
+        print header + data
+    
